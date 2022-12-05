e689fff5fc6e98a7d7cdafd24d225c4e71f4273b
Tiziano Mele <tiziano.mele09@gmail.com>
2022-12-05 22:38:52 +0100

    retrieve data for serie a, bundesliga, premier league.

--- MatchPrediction/Services/MatchStatsGetterService/Impl/FootballDataCoUkGetter.cs
+++ MatchPrediction/Services/MatchStatsGetterService/Impl/FootballDataCoUkGetter.cs
@@ -1,9 +1,9 @@
-﻿using CsvHelper;
-using CsvHelper.Configuration;
+﻿using CsvHelper.Configuration.Attributes;
 using MatchPrediction.Helpers.CsvHelper;
 using MatchPrediction.Models.MatchPrediction;
 using System.Globalization;
-using System.IO;
+using static System.Linq.Enumerable;
+using static System.Net.WebRequestMethods;
 
 namespace MatchPrediction.Services.MatchStatsGetterService.Impl
 {
@@ -13,6 +13,15 @@ namespace MatchPrediction.Services.MatchStatsGetterService.Impl
         private readonly ICsvHelper<MatchDto> _csvreader;
         private readonly IHttpClientFactory _httpClientFactory;
 
+        private readonly List<string> _leagues = new List<string>
+        {
+            "I1", // Italy serie A
+            "E0", // England 
+            "D1"  // Germany
+        };
+        private readonly int _fromYear;
+        private readonly int _toYear;
+
         public FootballDataCoUkGetter(
                 ILogger<FootballDataCoUkGetter> logger, 
                 ICsvHelper<MatchDto> csvreader,
@@ -21,25 +30,41 @@ namespace MatchPrediction.Services.MatchStatsGetterService.Impl
             _logger = logger;
             _csvreader = csvreader;
             _httpClientFactory = httpClientFactory;
+            
+            // Leagues I want to import
+            _leagues = new List<string>
+            {
+                "I1", // Italy serie A
+                "E0", // England 
+                "D1"  // Germany
+            };
+            // Only from 2018 to 2022
+            _fromYear = 18;
+            _toYear= 22;
         }
 
         public async Task<IEnumerable<Match>> GetMatchesStats()
         {
             var http = _httpClientFactory.CreateClient();
-
-            var response = await http.GetAsync("https://www.football-data.co.uk/mmz4281/2021/I1.csv");
-            response.EnsureSuccessStatusCode();
-
-            var responseContentStream = await response.Content.ReadAsStreamAsync();
-
-            var matchesdto = _csvreader.Read(responseContentStream);
-
             var matches = new List<Match>();
-            foreach (var item in matchesdto)
-            {
-                matches.Add(item.ToMatch());
-            }
 
+            foreach (var league in _leagues)
+            {
+                foreach (var year in Range(_fromYear, (_toYear - _fromYear)))
+                {
+                    var urlToCall = "https://www.football-data.co.uk/mmz4281/" + year.ToString() + (year + 1).ToString() + "/" + league + ".csv";
+                    var response = await http.GetAsync(urlToCall);
+                    response.EnsureSuccessStatusCode();
+
+                    var responseContentStream = await response.Content.ReadAsStreamAsync();
+
+                    var matchesdto = _csvreader.Read(responseContentStream);
+                    foreach (var item in matchesdto)
+                    {
+                        matches.Add(item.ToMatch());
+                    }
+                }
+            }
             return matches;
         }
     }
@@ -49,6 +74,9 @@ public sealed class MatchDto
 {
     public string Div { get; set; }
     public string Date { get; set; }
+
+    [Optional]
+    public string Time { get; set; }
     public string HomeTeam { get; set; }
     public string AwayTeam { get; set; }
     public int FTHG { get; set; }
@@ -59,7 +87,7 @@ public sealed class MatchDto
         var match = new Match()
         {
             Div = Div,
-            Date = DateTime.ParseExact(Date, "dd/MM/yyyy", CultureInfo.InvariantCulture),
+            Date = Time == null ? DateTime.ParseExact(Date, "dd/MM/yyyy", CultureInfo.InvariantCulture) : DateTime.ParseExact(Date + " " + Time, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture),
             HomeTeam = HomeTeam,
             AwayTeam = AwayTeam,
             FTHG = FTHG,
