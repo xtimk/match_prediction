a21224877c4de0d562b399b2a287cc467702330d
Tiziano Mele <tiziano.mele09@gmail.com>
2022-12-05 23:29:38 +0100

    add getmatches api and datatable view.

--- MatchPrediction/Controllers/HomeController.cs
+++ MatchPrediction/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using MatchPrediction.Models;
+﻿using MatchPrediction.Data.Contexts;
+using MatchPrediction.Helpers.DataTables;
+using MatchPrediction.Models;
 using MatchPrediction.Services.MatchStatsGetterService;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace MatchPrediction.Controllers
@@ -9,18 +12,34 @@ namespace MatchPrediction.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IMatchStatsGetterService _matchStatsGetterService;
+        private readonly MatchPredictionContext _db;
 
         public HomeController(
                 ILogger<HomeController> logger,
-                IMatchStatsGetterService matchStatsGetterService)
+                IMatchStatsGetterService matchStatsGetterService,
+                MatchPredictionContext db)
         {
             _logger = logger;
             _matchStatsGetterService = matchStatsGetterService;
+            _db = db;
         }
 
         public async Task<IActionResult> Index()
         {
+            
             return View();
+        }
+
+        public async Task<JsonResult> GetMatches(DataTableAjaxPostModel model)
+        {
+            var result = await _db.Matches.Where(x => x.Div == "I1").Take(100).ToListAsync();
+            return Json(new
+            {
+                draw= model.draw,
+                recordsTotal = result.Count,
+                recordsFiltered = 0,
+                data = result
+            });
         }
 
         public IActionResult Privacy()
